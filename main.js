8f38f3c0e58ec7975cef6a5668231d4dd7dcf370
Md adnanul Chowdhury <adnanul1993@gmail.com>
2018-04-02 17:17:45 -0400

    [2nd time] edited sticky navigation bar code. added comments to sticky navigation bar code.

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // window.scroll code structure was taken from W3 School "How TO - Sticky/Affix Navbar"
 // It was used to create the sticky navigation menu while a user is scrolling down page. The function name, variable name, and class names have been changed.
 // links: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+//window.onscroll excutes the function "sticky_navigation_menu" while the user is scrolling a page.
+//The function "sticky_navigation_menu" grabs the navigation bar its id called "navigation_bar" and sticks the menu bar to the top when a user scrolls down the page.
+// When a user scrolls back up to the top of the page the function removes the sticky top down menu bar.
 window.onscroll = function () { sticky_navigation_menu () };
 
 let navigation_menu = document.getElementById("navigation_bar");
